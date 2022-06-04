9e5eb86579cc9f2dd1e60539a64a418eb2c4ce19
hamedsz <badmansog@gmail.com>
2022-06-04 23:59:10 +0430

    test task update status

---
+++ tests/Feature/TaskUpdateStatusTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace TodoApp\Tests\Feature;
+
+use TodoApp\app\Models\Task;
+use TodoApp\Tests\TestCase;
+
+class TaskUpdateStatusTest extends TestCase
+{
+    public function testTaskUpdateStatus(){
+        $this->auth();
+        $task = $this->createFakeTask();
+
+        $response = $this->json('PUT', '/api/v1/todo/tasks/'. $task->id . '/update-status', [
+            'status' => Task::STATUS_TASK_CLOSE
+        ]);
+
+        $response->assertStatus(204);
+
+        $task = Task::find($task->id);
+        $this->assertEquals($task->status, Task::STATUS_TASK_CLOSE);
+    }
+}
