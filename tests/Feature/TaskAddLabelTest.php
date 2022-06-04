b30b72ad850b3ada64f3958c7e143ad577603f37
hamedsz <badmansog@gmail.com>
2022-06-05 00:13:15 +0430

    add label to task tests

---
+++ tests/Feature/TaskAddLabelTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace TodoApp\Tests\Feature;
+
+use TodoApp\app\Models\Task;
+use TodoApp\Tests\TestCase;
+
+class TaskAddLabelTest extends TestCase
+{
+    public function testTaskAddLabel(){
+        $this->auth();
+        $task = $this->createFakeTask();
+
+        $response = $this->json('PUT', '/api/v1/todo/tasks/'. $task->id . '/add-label', [
+            'labels' => [
+                'china',
+                'usa'
+            ]
+        ]);
+
+        $response->assertStatus(204);
+
+        $this->assertEquals($task->labels()->count(), 2);
+        $this->assertEquals($task->labels[0]->label, 'china');
+        $this->assertEquals($task->labels[1]->label, 'china');
+    }
+}
