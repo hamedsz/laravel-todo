bf3495eb680352a73ee73ae78fbc77061ea312d9
hamedsz <badmansog@gmail.com>
2022-06-04 22:16:17 +0430

    test create tasks:

---
+++ tests/Feature/TaskCreateTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace TodoApp\Tests\Feature;
+
+use TodoApp\app\Models\Task;
+use TodoApp\Tests\TestCase;
+
+class TaskCreateTest extends TestCase
+{
+    public function testCreateTask(){
+        $this->auth();
+
+        $data = [
+            'title' => 'hello',
+            'description' => 'hi',
+            'labels' => [
+                'hamed',
+                'sz'
+            ]
+        ];
+
+        $response = $this->json('POST', '/api/v1/todo/tasks/', $data);
+        $response->assertStatus(204);
+
+
+        $task = Task::query()->first();
+        $this->assertNotNull($task);
+
+        if (!$task){
+            return;
+        }
+
+        $this->assertEquals($task->title, $data['title']);
+        $this->assertEquals($task->description, $data['description']);
+        $this->assertEquals($task->labels()->count(), count($data['labels']));
+        $this->assertEquals($task->labels[0]->label, 'hamed');
+        $this->assertEquals($task->labels[0]->label, 'sz');
+    }
+}
