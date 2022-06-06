651f02be419ff1331d40027d31f9a9e93b231bb5
hamedsz <badmansog@gmail.com>
2022-06-06 13:28:48 +0430

    signup tests complete

--- tests/Feature/SignupTest.php
+++ tests/Feature/SignupTest.php
@@ -21,4 +21,28 @@ class SignupTest extends TestCase
                 ]
             ]);
     }
+
+    public function testSignupIncorrectConfirmation(){
+        $response = $this->json('POST', '/api/v1/todo/auth/signup', [
+            'name' => 'TEST',
+            'email' => 'example@mail.com',
+            'password' => 'password',
+            'password_confirmation' => 'password2',
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function testSignupDuplicateEmail(){
+        $user = $this->createFakeUser();
+
+        $response = $this->json('POST', '/api/v1/todo/auth/signup', [
+            'name' => 'TEST',
+            'email' => $user->email,
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertStatus(422);
+    }
 }
